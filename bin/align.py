c50f6a4f0f2ff5af19094183487f8ad96f435e1f
Carlos Arteta <carlos.arteta@optellum.com>
2023-07-24 23:07:33 +0100

    Refactors config file

--- bin/align.py
+++ bin/align.py
@@ -24,46 +24,109 @@ def parse_cli_args():
         config = yaml.safe_load(config_fp)
 
     # Validate arguments
-    if 'images' not in config:
-        raise ValueError("'images' must be specified")
-
-    if not os.path.exists(config['images']):
-        raise ValueError(f"{config['images']} does not exist")
+    if 'root_path' not in config:
+        raise ValueError("'root_path' must be specified")
     
-    if 'densities' not in config and 'locations' not in config:
-        raise ValueError("Either 'densities' or 'locations' must be specified")
+    if 'camera_name' not in config:
+        raise ValueError("'camera_name' must be specified")
     
-    if 'densities' in config and 'locations' in config:
-        raise ValueError("Only one of 'densities' or 'locations' must be specified")
+    if 'camera_collection_id' not in config:
+        raise ValueError("'camera_collection_id' must be specified")
+    
+    root_path = config['root_path']
+    camera_name = config['camera_name']
+    camera_collection_id = config['camera_collection_id']
 
-    if 'densities' in config and not os.path.exists(config['densities']):
-        raise ValueError(f"{config['densities']} does not exist")
+    images_path = os.path.join(
+        root_path, 
+        camera_name, 
+        f'{camera_name}_renamed', 
+        f'{camera_name}{camera_collection_id}_renamed'
+    )
 
+    if not os.path.exists(images_path):
+        raise ValueError(f"{images_path} does not exist")
+
+    densities_path = os.path.join(
+        root_path, 
+        camera_name, 
+        f'{camera_name}_pengbot', 
+        f'{camera_name}{camera_collection_id}_pengbot'
+    )
+
+    if not os.path.exists(densities_path):
+        densities_path = None
+        locations_path = os.path.join(
+                root_path, 
+                camera_name, 
+                f'{camera_name}_metadata', 
+                f'{camera_name}{camera_collection_id}_locations.csv'
+            )
+        if not os.path.exists(locations_path):
+            raise ValueError(f"Either {densities_path} or {locations_path} must exist")
+        if 'location_mask_size' not in config:
+                warnings.warn("'location_mask_size' not specified, defaulting to 100", UserWarning)
+                config['location_mask_size'] = 100
+    else:
+        locations_path = None
+        location_mask_size = None
+
+    metadata_path = os.path.join(
+        root_path, 
+        camera_name, 
+        f'{camera_name}_metadata', 
+        f'{camera_name}{camera_collection_id}_metadata'
+    )
+
+    if 'months_to_process' not in config:
+        config['months_to_process'] = []
+
+    months_to_process = config['months_to_process']
+
+    if not os.path.exists(metadata_path) and len(months_to_process) > 0:
+        raise ValueError(f"Months {months_to_process} requested but {metadata_path} does not exist")
+  
     if 'nest_diagram' not in config:
         raise ValueError("'nest_diagram' must be specified")
 
     if not os.path.exists(config['nest_diagram']):
         raise ValueError(f"{config['nest_diagram']} does not exist")
 
-    if 'nest_reference_image' not in config:
-        raise ValueError("'nest_reference_image' must be specified")
+    if 'nest_reference_image_id' not in config:
+        raise ValueError("'nest_reference_image_id' must be specified")
     
-    if not os.path.exists(config['nest_reference_image']):
-        raise ValueError(f"{config['nest_reference_image']} does not exist")
+    nest_reference_image_id = config['nest_reference_image_id']
+    nest_reference_image_path = os.path.join(
+        images_path,
+        f'{camera_name}{camera_collection_id}_{nest_reference_image_id}.JPG'
+    )
+    
+    if not os.path.exists(nest_reference_image_path):
+        raise ValueError(f"{nest_reference_image_path} does not exist")
+   
+    nest_reference_density_path = os.path.join(
+            densities_path,
+            f'{camera_name}{camera_collection_id}_{nest_reference_image_id}.mat'
+        )
 
-    if 'nest_reference_density' not in config and 'locations' not in config:
-        raise ValueError("Either 'nest_reference_density' or 'locations' must be specified")
-
-    if 'nest_reference_density' in config and not os.path.exists(config['nest_reference_density']):
-        raise ValueError(f"{config['nest_reference_density']} does not exist")
+    if not os.path.exists(nest_reference_density_path):
+        raise ValueError(f"{nest_reference_density_path} does not exist")
     
     if 'fill_missing' not in config:
         warnings.warn("'fill_missing' not specified, defaulting to True", UserWarning)
         config['fill_missing'] = True
 
-    if 'location_mask_size' not in config and 'locations' in config:
-        warnings.warn("'location_mask_size' not specified, defaulting to 100", UserWarning)
-        config['location_mask_size'] = 100
+    if 'camera_info_size' not in config:
+        warnings.warn("'camera_info_size' not specified, defaulting to 50", UserWarning)
+        config['camera_info_size'] = 50
+
+    config['images'] = images_path
+    config['densities'] = densities_path
+    config['locations'] = locations_path
+    config['location_mask_size'] = location_mask_size
+    config['metadata'] = metadata_path
+    config['nest_reference_image'] = nest_reference_image_path
+    config['nest_reference_density'] = nest_reference_density_path
 
     return config
 
@@ -74,16 +137,16 @@ def main():
     # create object that processes a folder of images for alignment
     folder_processor = processor.FolderProcessor(
         images_dir=config['images'],
-        densities_dir=config['densities'] if 'densities' in config else None,
-        locations_path=config['locations'] if 'locations' in config else None,
-        bounding_box_size=config['location_mask_size'] if 'location_mask_size' in config else None,
-        camera_info_size=config['camera_info_size'] if 'camera_info_size' in config else 50,
+        densities_dir=config['densities'],
+        locations_path=config['locations'],
+        bounding_box_size=config['location_mask_size'],
+        camera_info_size=config['camera_info_size'],
         fill_missing=config['fill_missing']
     )
 
     ref_im_unit = folder_processor.im_unit_from_paths(
         im_path=config['nest_reference_image'],
-        density_path=config['nest_reference_density'] if 'nest_reference_density' in config else None,
+        density_path=config['nest_reference_density'] if config['densities'] is not None else None,
         diagram_path=config['nest_diagram']
     )
     folder_processor.add_reference(ref_im_unit)
